4f38fd9350a4116895613b0b79e3fde5d60ca889
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-04-21 23:30:48 -0300

    Trazendo DOM

---
+++ Pilotando com DOM/script.js
@@ -0,0 +1,51 @@
+// Pegando um elemento HTML através do ID
+
+// let valorh1 = document.getElementById("teste");
+// console.log(valorh1);
+
+// Pegando um elemento HTML através da Classe
+
+// let valores = document.getElementsByClassName("um");
+// console.log(valores);
+
+// Pegando elementos HTML através das Tag
+
+// let tags = document.getElementsByTagName("h2");
+// console.log(tags);
+
+//Pegando elementos HTML através do QuerySelector - Provavel melhor modo
+
+// let query = document.querySelector(".um");
+// console.log(query);
+
+//Pegando elementos HTML pelo QuerySelectorAll
+
+// Igual a o queryselector porém podemos rodar um foreach() nele
+//enquanto nos getElementsById ou Class não podemos
+
+// let querydoc = document.querySelector("#teste");
+// querydoc.textContent = "Cansei de testes";
+
+// querydoc.innerText = "Talvez mais um";
+
+// querydoc.innerHTML = "Mais um pra fechar o dia <h2>Acredito em mim </h2>";
+
+// const element = document.querySelector("input");
+
+// // element.value = "bom dia familia";
+// console.log(element.value);
+
+// Adicionando atributos em elemento HTML
+
+// const header = document.querySelector("header");
+
+// header.setAttribute("id", "header");
+
+// const headerID = document.querySelector("#header");
+// console.log(headerID);
+
+// console.log(headerID.getAttribute("id"));
+
+// console.log(headerID.removeAttribute("id"));
+
+export function hello() {}
