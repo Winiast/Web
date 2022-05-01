f4cb4642de8cf6c6059054e9a8402c9676a84470
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-05-01 14:43:40 -0300

    DOM

--- Pilotando com DOM/eventos.js
+++ Pilotando com DOM/eventos.js
@@ -1,12 +1,29 @@
-function print() {
-  return bomDia();
-}
+// function print() {
+//   return bomDia();
+// }
 
-function bomDia() {
-  let h1 = document.createElement("h1");
-  h1.innerText = "Bom dia Usuario";
+// function bomDia() {
+//   let h1 = document.createElement("h1");
+//   h1.innerText = "Bom dia Usuario";
 
-  let body = document.querySelector("body");
-  let header = document.querySelector("header");
-  body.insertBefore(h1, header);
-}
+//   let body = document.querySelector("body");
+//   let header = document.querySelector("header");
+//   body.insertBefore(h1, header);
+// }
+
+// const input = document.querySelector("input");
+// // onkeydown = ao apertar a tecla já aciona
+// // onkeyup = só aciona ao soltar a tecla
+// input.onkeydown = () => {
+//   console.log("Ola");
+// };
+
+// input.onkeyup = () => {
+//   console.log("ate");
+// };
+
+const h2 = document.querySelector("h2");
+
+h2.addEventListener("click", () => {
+  console.log("teste");
+});
